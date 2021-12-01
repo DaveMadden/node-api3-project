4ab16bfb93fb0811e2a5b401a27624d5e296b9bd
Dave Madden <davelearnsstuff@gmail.com>
2021-12-01 14:22:32 -0800

    posting

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -27,15 +27,21 @@ router.get('/:id', validateUserId, (req, res) => {
   res.status(200).json(req.user);
 });
 
-router.post('/', (req, res) => {
-  // RETURN THE NEWLY CREATED USER OBJECT
-  // this needs a middleware to check that the request body is valid
+router.post('/', validateUser, (req, res) => {
+  User.insert(req.body)
+  .then(response =>{
+    res.status(201).json(response);
+  })
+    .catch(error =>{
+      res.status(500).json({message: `${error}`});
+    })
 });
 
-router.put('/:id', validateUserId, (req, res) => {
+router.put('/:id', validateUserId, validateUser (req, res) => {
   // RETURN THE FRESHLY UPDATED USER OBJECT
   // this needs a middleware to verify user id
   // and another middleware to check that the request body is valid
+  
 });
 
 router.delete('/:id', validateUserId, (req, res) => {
